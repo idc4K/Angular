35d03285e509ef5cd8d99b801f50a8992ef5fba6
idc4k <christikonga83@gmail.com>
2023-03-13 15:14:03 +0100

    observale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit} from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { filter, interval,Observable } from 'rxjs';
 import { Data } from './module/snap.mode';
+import {map, tap} from 'rxjs/operators'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,20 @@ import { Data } from './module/snap.mode';
 })
 export class AppComponent implements OnInit {
 
-  interval$ ! : Observable<number>
-  ngOnInit(){
-   this.interval$ = interval(1000)
+  interval$ !: Observable<string>
+  constructor(private router: Router){}
+  ngOnInit() {
+
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 == 0 ? `${value} nombre pair` : `${value} nombre impair`),
+      tap(text => this.Logger(text))
+    )
   } 
+
+  Logger(text : string){
+    console.log(`Log : ${text}`)
+  }
   onClick(){
 
   }
